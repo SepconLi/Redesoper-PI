5d3ef43ec800fd4cee970abe4edbd604e1d81508
Rodrigo <rodrigo.ucr.ac.cr>
2021-09-14 18:51:30 -0600

    Read y Write

--- Syscalls/Read.c
+++ Syscalls/Read.c
@@ -1,12 +1,17 @@
+#ifndef READ_H
+#define READ_H
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stddef.h>
 
-int main(int argc, char* argv[]){
-    int fd = open(argv[1],50);
+int read(int fd, const void *buf, int bytes) {
+    fd = open(buf,bytes);
     if(fd == -1) {
-       printf("ERROR, FILE NOT FOUND.\n");
        return 1;
+    } else {
+        mmap(NULL,bytes,PROT_READ,MAP_SHARED,fd,0);
     }
     return 0;
 }
+#endif
